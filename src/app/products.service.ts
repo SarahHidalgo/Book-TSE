e0410be4988165a62158147df6b7517eb4d33475
Sarah <sarahhidalgo.sh@gmail.com>
2023-11-07 17:08:40 +0100

    affiche test products api

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Products } from "./interfaces/products";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ProductsService {
-  productsUrl = "assets/products.json";
-
+  productsUrl : string = "assets/products.json";
   constructor(private http: HttpClient) { }
 
-  getProducts(){
-    return this.http.get<Products>(this.productsUrl);
+  getProducts = () => {
+    return this.http.get<Array<Products>>(this.productsUrl);
   }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { Products } from '../interfaces/products';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
 
+export class ProductsComponent implements OnInit {
+
+  products: Array<Products> = [];
+
+  constructor(private productService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+      console.log(data);
+      
+    });
+  }
 }
