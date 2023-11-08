a56b8282a15c989b712b28f3c436aa281a27522e
Sarah <sarahhidalgo.sh@gmail.com>
2023-11-08 21:11:49 +0100

    details of products

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+//product-detail.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  product: any;
+
+  constructor(private route: ActivatedRoute, private productsService: ProductsService) {
+  }
+
+ngOnInit() {
+    const idString = this.route.snapshot.paramMap.get('id');
+    if (idString !== null) {
+      const id = parseInt(idString); // Convertir la chaîne en nombre
+      this.productsService.getProduct(id).subscribe(product => {
+        this.product = product;
+      });
+    } else {
+      console.log("l'id est nul");
+      // Gérer le cas où l'ID est null, par exemple rediriger ou afficher un message d'erreur.
+    }
+  }
+ 
 
 }
