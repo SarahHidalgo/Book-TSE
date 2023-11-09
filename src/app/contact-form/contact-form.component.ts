7f6fb4e648031c5528b4bf95755bb23bd892e929
Sarah <sarahhidalgo.sh@gmail.com>
2023-11-09 12:56:17 +0100

    contact form creation

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+// contact-form.component.ts
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent {
+export class ContactFormComponent implements OnInit{
+  ngOnInit(): void {
+  }
+  name: string = '';
+  email: string = '';
+  message: string = '';
+
+  validateForm() {
+    if (this.email && this.message) {
+      // Envoyer le formulaire
+      console.log('Formulaire valide, envoyez-le !');
+    } else {
+      console.log('Veuillez remplir tous les champs.');
+    }
+  }
+
 
 }
